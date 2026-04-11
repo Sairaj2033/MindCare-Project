370cd1bb6e598b1213169740df8f0847d04564c1
Sairaj Patil <sairajpatil035@gmail.com>
2026-04-12 03:52:34 +0530

    fix: restore missing imports in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 import path from "path";
 import { fileURLToPath } from "url";
 
